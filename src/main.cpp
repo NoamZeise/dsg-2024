c391c03cc444e8a9371576d4d11362e9dacc60aa
Noam Zeise <noam4134@gmail.com>
2024-02-23 18:37:44 +0000

    inital commits, added graphics lib

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <manager.h>
+#include <game/camera.h>
+#include <graphics/glm_helper.h>
+#include <glm/gtc/matrix_inverse.hpp>
+
+// This example shows minimum functionality of the graphics environment
+// With none of the optional libs
+//
+// The program draws a textured quad
+// Press Esc to exit.
+
+int main() {
+    ManagerState state;
+    state.cursor = cursorState::disabled;
+    state.windowTitle = "minimum example";
+
+    Manager manager(state);
+    ResourcePool* pool = manager.render->pool();
+
+    Resource::Texture tex = pool->tex()->load("textures/tile.png");
+
+    manager.render->LoadResourcesToGPU(pool);
+    manager.render->UseLoadedResources();
+
+    while(!glfwWindowShouldClose(manager.window)) {
+	manager.update();
+	
+	if(manager.input.kb.press(GLFW_KEY_ESCAPE))
+	    glfwSetWindowShouldClose(manager.window, GLFW_TRUE);
+	
+	if(manager.winActive()) {
+            manager.render->DrawQuad(tex,
+				     glmhelper::calcMatFromRect(
+					     glm::vec4(10.0f, 10.0f, 200.0f, 200.0f),
+					     0.0f, 0.5f),
+				     glm::vec4(1.0f),
+				     glmhelper::getTextureOffset(
+					     tex.dim,
+					     glm::vec4(0.0f, 0.0f, tex.dim.x * 5, tex.dim.y * 5)));
+	    manager.render->EndDraw();
+	}
+    }
+}
